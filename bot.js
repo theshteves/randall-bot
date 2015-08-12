be97ea1620a341c4cb938e1ec120181f49f89143
theshteves <kneisers@msu.edu>
2015-08-11 19:23:40 -0700

    Initial bot setup

---
+++ bot.js
@@ -0,0 +1,46 @@
+// bot.js
+
+var Slack = require("slack-client");
+var keys = require("./keys.js");
+
+var token = keys.token;
+var slack = new Slack(token, true, true);
+
+
+slack.on('open', function () {
+    var channels = Object.keys(slack.channels)
+	.map(function (k) { return slack.channels[k]; })
+	.filter(function (c) { return c.is_member; })
+	.map(function (c) { return c.name; });
+
+    var groups = Object.keys(slack.groups)
+	.map(function (k) { return slack.groups[k]; })
+	.filter(function (g) { return g.is_open && !g.is_archived; })
+	.map(function (g) { return g.name; });
+
+    console.log('Welcome to Slack. You are ' + slack.self.name + ' of ' + slack.team.name);
+
+    if (channels.length > 0) {
+	console.log('You are in: ' + channels.join(', '));
+    }
+    else {
+	console.log('You are not in any channels.');
+    }
+
+    if (groups.length > 0) {
+	console.log('As well as: ' + groups.join(', '));
+    }
+});
+
+slack.login();
+
+slack.on('message', function(message) {
+    var channel = slack.getChannelGroupOrDMByID(message.channel);
+    var user = slack.getUserByID(message.user);
+
+    if (message.type === 'message') {
+	console.log(channel.name + ':' + user.name + ':' + message.text);
+    }
+
+    //Insert commands here
+});
