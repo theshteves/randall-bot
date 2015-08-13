ffd14d43e5019dd24f47b511626bc6e8cbf2378f
theshteves <kneisers@msu.edu>
2015-08-13 13:13:06 -0700

    fixed bot crash after posting link

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ var slack = new Slack(token, true, true);
 
 // Initialize island
 var island = {};
-
+var mainland = {};
 
 slack.on('open', function () {
     var channels = Object.keys(slack.channels)
@@ -35,9 +35,14 @@ slack.on('open', function () {
     }
 
 
-    // Fill island with players
+    // Fill mainland with all players
+    slack.channels.C08V4MUSF.members.forEach(function(person_id) {
+	    mainland[slack.getUserByID(person_id).name] = [];
+    });
+
+    // Fill island with #yoshis-story players
     slack.channels.C08V4V25T.members.forEach(function(person_id) {
-	    island[slack.getUserByID(person_id).name] = [[], [], []];
+	    island[slack.getUserByID(person_id).name] = [[], []];
     });
     console.log("\n[=====[THE ISLAND]=====]\n", island, "\n");
 });
@@ -47,96 +52,144 @@ slack.login();
 
 
 slack.on('message', function(message) {
-    var channel = slack.getChannelGroupOrDMByID(message.channel);
-    var user = slack.getUserByID(message.user);
+    // Catch errors
+    try {
 
 
-    // Log message
-    if (message.type === 'message') {
-	try {
+	var channel = slack.getChannelGroupOrDMByID(message.channel);
+	var user = slack.getUserByID(message.user);
+
+
+	// Log message
+	if (message.type === 'message') {
 	    console.log("[#" + channel.name + "] " + user.name + " | " + message.text);
-	} catch (err) {} // For "user is not defined" error
-    }
+	}
 
 
-    // Verifies if text represents player
-    var isUser = function(text) {
-	if (island.hasOwnProperty(text)) {
+	// command - debug
+	if (message.text.substring(0,4) == "help") {
+	    console.log(slack);
+	}
+
+
+	// Verifies if text represents player
+	var isUser = function(text) {
+	    if (island.hasOwnProperty(text)) {
+		return true;
+	    }
+	    return false;
+	};
+
+
+	// kicks user
+	var kickUser = function(person) {
+	    var DIALOGUES = ["_a screach was heard in the distance__",
+			     "_they must atone for their sins__",
+			     "_there was not a sound to be heard in the jungle_"];
+	    var r = true; //random number
+
+
+	    /* --- actually kicks user --- */
+	    channel.send(ENDING(r));
 	    return true;
+	};
+
+
+	// command - status
+	if (message.text.substring(0,6) == "status" && isUser(message.text.substring(7))) {
+	    var usr = message.text.substring(7);
+	    channel.send(":yoshiegg: " + usr + " :yoshiegg:\n"
+			 + "upvotes: " + island[usr][0] + "\n"
+			 + "downvotes: " + island[usr][1]);
 	}
-	return false;
-    };
 
 
-    // command - status
-    if (message.text.substring(0,6) == "status" && isUser(message.text.substring(7))) {
-	var usr = message.text.substring(7);
-	channel.send(":yoshiegg: " + usr + " :yoshiegg:\n"
-		     + "upvotes: " + island[usr][0] + "\n"
-		     + "downvotes: " + island[usr][1]);
-    }
-
-
-    // command - upvote
-    if (message.text.substring(0,9) == ":randall:" && isUser(message.text.substring(10))) {
-	// submit upvote
-	console.log("[" + message.text.substring(10)  + "]  +1 from " + user.name);
-	if (island[message.text.substring(10)][0].indexOf(user.name, 0) == -1) {
-	    island[message.text.substring(10)][0].push(user.name);
+	// command - upvote
+	if (message.text.substring(0,9) == ":randall:" && isUser(message.text.substring(10))) {
+	    // submit upvote
+	    console.log("[" + message.text.substring(10)  + "]  +1 from " + user.name);
+	    if (island[message.text.substring(10)][0].indexOf(user.name, 0) == -1) {
+		island[message.text.substring(10)][0].push(user.name);
+	    }
+	    // remove downvote if necessary
+	    if (island[message.text.substring(10)][1].indexOf(user.name, 0) != -1) {
+		island[message.text.substring(10)][1].splice(island[message.text.substring(10)][1].indexOf(user.name), 1);
+	    }
 	}
-	// remove downvote if necessary
-	if (island[message.text.substring(10)][1].indexOf(user.name, 0) != -1) {
-	    island[message.text.substring(10)][1].splice(island[message.text.substring(10)][1].indexOf(user.name), 1);
+
+
+	// command - downvote
+	if (message.text.substring(0,7) == ":yoshi:" && isUser(message.text.substring(8))) {
+	    // submit downvote
+	    console.log("[" + message.text.substring(8)  + "]  -1 from " + user.name);
+	    if (island[message.text.substring(8)][1].indexOf(user.name, 0) == -1) {
+		island[message.text.substring(8)][1].push(user.name);
+	    }
+	    // remove upvote if necessary
+	    if (island[message.text.substring(8)][0].indexOf(user.name, 0) != -1) {
+		island[message.text.substring(8)][0].splice(island[message.text.substring(8)][0].indexOf(user.name), 1);
+	    }
+	    // kick user off island
+	    if (island[message.text.substring(8)][1].length - island[message.text.substring(8)][0] >= 5) {
+		console.log("[" + message.text.substring(8) + "]  has been voted off");
+		island[message.text.substring(8)][0] = 0;
+		island[message.text.substring(8)][1] = 0;
+		mainland[message.text.substring(8)] = 0;
+		if (message.text.substring(8) == "durr") {
+		    channel.send(":yoshi::yoshi::yoshi::yoshiegg: durr has been taken hostage :yoshiegg::yoshi::yoshi::yoshi:");
+		    channel.send(":yoshi::yoshi::yoshi::yoshiegg: durr must atone for his sins :yoshiegg::yoshi::yoshi::yoshi:");
+		    channel.send(":yoshi::yoshi::yoshi::yoshiegg: durr shall be judged :yoshiegg::yoshi::yoshi::yoshi:");
+		} else {
+		    channel.send(":yoshi::yoshi::yoshi::yoshiegg: " + message.text.substring(8) + " has been taken hostage :yoshiegg::yoshi::yoshi::yoshi:");
+		    kickUser(message.text.substring(8));
+		}
+	    }
 	}
-    }
 
 
-    // command - downvote
-    if (message.text.substring(0,7) == ":yoshi:" && isUser(message.text.substring(8))) {
-	// submit downvote
-	console.log("[" + message.text.substring(8)  + "]  -1 from " + user.name);
-	if (island[message.text.substring(8)][1].indexOf(user.name, 0) == -1) {
-	    island[message.text.substring(8)][1].push(user.name);
+
+	// command - summon
+	if (message.text.substring(0,10) == ":yoshiegg:" && isUser(message.text.substring(11))) {
+	    // submit summon
+	    console.log("[" + message.text.substring(8)  + "]  summon from " + user.name);
+	    if (mainland[message.text.substring(11)].indexOf(user.name, 0) == -1) {
+		mainland[message.text.substring(11)].push(user.name);
+	    }
+	    // execute summon
+	    if (mainland[message.text.substring(11)] == 3) {
+		console.log("[" + message.text.substring(11) + "]  has been summoned");
+		//message.text.substring(11)
+	    }
 	}
-	// remove upvote if necessary
-	if (island[message.text.substring(8)][0].indexOf(user.name, 0) != -1) {
-	    island[message.text.substring(8)][0].splice(island[message.text.substring(8)][0].indexOf(user.name), 1);
+
+
+	/*
+	  durr-specific commands
+	*/
+
+
+	// shameless self-promotion
+	if (message.text.indexOf("Face The Falcon", 0) != -1) {
+	    channel.send("https://www.youtube.com/watch?v=YXPLysfBeag");
 	}
-    }
-
-
-    // command - summon
-    if (message.text.substring(0,10) == ":yoshiegg:" && isUser(message.text.substring(11))) {
-	// submit summon
-	console.log("[" + message.text.substring(8)  + "]  summon from " + user.name);
-	if (island[message.text.substring(11)][2].indexOf(user.name, 0) == -1) {
-	    island[message.text.substring(11)][2].push(user.name);
+	if (message.text.indexOf("Embrace The Falcon", 0) != -1) {
+	    channel.send("https://www.youtube.com/watch?v=fG982Lt-F7k");
 	}
-    }
-
-    /*
-      durr-specific commands
-    */
-
-    // Shameless self-promotion
-    if (message.text.indexOf("Face The Falcon", 0) != -1) {
-	channel.send("https://www.youtube.com/watch?v=YXPLysfBeag");
-    }
-    if (message.text.indexOf("Embrace The Falcon", 0) != -1) {
-	channel.send("https://www.youtube.com/watch?v=fG982Lt-F7k");
-    }
 
 
-    // command - "Who's your daddy?"
-    if (message.text.indexOf("Who's your daddy?", 0) != -1) {
-	console.log(user);
-	if (user == "durr") {
-	    channel.send("You're my daddy :randall:");
-	} else {
-	    channel.send("durr's my daddy :randall:");
+	// command - "Who is your daddy?"
+	if (message.text.toLowerCase().indexOf("who is your daddy?", 0) != -1) {
+	    if (user.name == "durr") {
+		channel.send("You're my daddy :randall:");
+	    } else {
+		channel.send("durr's my daddy :randall:");
+	    }
 	}
-    }
 
 
-
+	// command - "SD"
+	if (message.text.toUpperCase().indexOf("SD", 0) != -1) {
+	    channel.send(user.name + "   ...I got you, bro");
+	}
+    } catch (err) {}
 });
