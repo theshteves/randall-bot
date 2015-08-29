0e4439ff166444b313ec6d2661e9aeb1cbdbbe7c
theshteves <kneisers@msu.edu>
2015-08-29 05:30:58 -0400

    Whoops! time to re-issue tokens

--- keys.js
+++
@@ -1,7 +0,0 @@
-// keys.js
-
-var api_token = "xoxb-9805616086-CI0UZUDFrpeZhSXxYVDirJpd";
-var admin_token = "xoxp-5199131090-6040919457-9806431428-2b26d0";
-
-exports.api_token = api_token;
-exports.admin_token = admin_token;
